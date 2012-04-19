952693af87456f30df3f11f3a8b23e9280f70e8f
Cody Dracars <cdracars@gmail.com>
2012-04-19 14:24:16 -0500

    added in operating system check.  Just Centos and Debian for now.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,21 @@
 class iceweasel::install {
-  package {
-    'iceweasel':
-      ensure => installed,
-      require => Class['xvfb'];
-  }
+    case $::operatingsystem {
+        'centos': {
+            package {
+                'firefox':
+                ensure => installed,
+                require => Class['xvfb'];
+            }
+        }
+        'debian': {
+            package {
+                'iceweasel':
+                ensure => installed,
+                require => Class['xvfb'];
+            }
+        }
+        default: { fail("No such operatingsystem: ${::operatingsystem} yet defined") }
+    }
 }
 
 class iceweasel {
